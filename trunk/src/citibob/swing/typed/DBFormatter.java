d42e5656d5cc883f75a6eda5ce15f3ae0db121a1
citibob <citibob@d6ec988e-330a-0410-a053-bb7e04c116e2>
2007-07-01 01:10:27 +0000

    An AbstractTextFormatter that requires a database lookup to do its valueToString().  This is used, for example, to display a name corresponding to an ID value providing a relationship between two records.  It can then be used in JTypedEditableLable or JTypedLabel.

---
+++ trunk/src/citibob/swing/typed/DBFormatter.java
@@ -0,0 +1,66 @@
+/*
+ * KeyedFormatter.java
+ *
+ * Created on March 18, 2006, 4:37 PM
+ *
+ * To change this template, choose Tools | Options and locate the template under
+ * the Source Creation and Management node. Right-click the template and choose
+ * Open. You can then make changes to the template in the Source Editor.
+ */
+
+package citibob.swing.typed;
+
+import citibob.util.KeyedModel;
+import javax.swing.*;
+import citibob.sql.*;
+import java.sql.*;
+import citibob.multithread.*;
+
+/**
+ * An AbstractFormatter that must make a database query to do its formatting.
+ * @author citibob
+ */
+public abstract class DBFormatter extends JFormattedTextField.AbstractFormatter
+{
+
+ConnPool pool;
+
+public DBFormatter(ConnPool pool)
+{
+	this.pool = pool;
+}
+
+/** Not to be used */
+public Object  stringToValue(String text)
+{
+	return text;
+}
+
+
+/** Override this. */
+public abstract String valueToString(Statement st, Object value)
+throws java.sql.SQLException;
+
+public String valueToString(Object value)
+{
+	String ret = null;
+	Statement st = null;
+	Connection dbb = null;
+	try {
+		dbb = pool.checkout();
+		st = dbb.createStatement();
+		ret = valueToString(st, value);
+	} catch(Throwable e) {
+		ret = e.toString();
+//		eh.consume(e);
+	} finally {
+		try {
+			if (st != null) st.close();
+		} catch(SQLException se) {}
+		try {
+			pool.checkin(dbb);
+		} catch(SQLException se) {}
+	}
+	return ret;
+}
+}
