dcafb35dfd64defca281ba1fc3e8b63f978cb6f2
citibob <citibob@d6ec988e-330a-0410-a053-bb7e04c116e2>
2007-09-12 13:58:35 +0000

    Used with XStream

---
+++ trunk/src/citibob/util/CircularArrayListConverter.java
@@ -0,0 +1,71 @@
+package citibob.util;
+/*
+ * CircularArrayListConverter.java
+ *
+ * Created on September 7, 2007, 1:09 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+import java.text.*;
+import java.util.*;
+import com.thoughtworks.xstream.*;
+import com.thoughtworks.xstream.converters.*;
+import com.thoughtworks.xstream.converters.collections.*;
+import com.thoughtworks.xstream.io.*;
+import com.thoughtworks.xstream.io.xml.*;
+import com.thoughtworks.xstream.mapper.*;
+
+import com.thoughtworks.xstream.*;
+
+public class CircularArrayListConverter extends CollectionConverter {
+public CircularArrayListConverter(Mapper mapper) { super(mapper); }
+
+        public boolean canConvert(Class clazz) {
+                return CircularArrayList.class.isAssignableFrom(clazz);
+        }
+
+        public void marshal(Object value, HierarchicalStreamWriter writer,
+                        MarshallingContext context) {
+			CircularArrayList o = (CircularArrayList)value;
+			writer.startNode("circularSize");
+			writer.setValue(""+o.circularSize);
+			writer.endNode();
+			super.marshal(value, writer, context);
+//			for (int i=0; i<o.size(); ++i) {
+//				writeItem(o.get(i), context, writer);
+//			}
+        }
+
+        public Object unmarshal(HierarchicalStreamReader reader,
+                        UnmarshallingContext context) {
+			reader.moveDown();
+			int size = Integer.parseInt(reader.getValue());
+			CircularArrayList arr = new CircularArrayList(size);
+			reader.moveUp();
+			populateCollection(reader, context, arr);
+			return arr;
+        }
+// =================================================================
+public static void main(String[] args) {
+	CircularArrayList a = new CircularArrayList(4);
+	a.addCircular((Integer)17);
+	a.addCircular((Integer)34);
+	a.addCircular((Integer)51);
+//	a.addCircular((Integer)68);
+//	a.addCircular((Integer)85);
+//	a.addCircular((Integer)102);
+//	a.addCircular((Integer)119);
+	XStream xstream = new XStream(new DomDriver());
+	xstream.registerConverter(new CircularArrayListConverter(xstream.getMapper()));
+	String xml = xstream.toXML(a);
+	System.out.println(xml);
+	Object obj = xstream.fromXML(xml);
+	
+	CircularArrayList a2 = (CircularArrayList)obj;
+	for (int i=0; i<a2.size(); ++i) {
+		System.out.println(a2.get(i));
+	}
+}
+}
